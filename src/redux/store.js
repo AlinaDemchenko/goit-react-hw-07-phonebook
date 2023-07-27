2a697bcfad81283a9c891e0b03baf20603c48073
Alina Demchenko <demalina88@gmail.com>
2023-07-27 11:58:06 -0700

    change

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './slice';
+import { contactsReducer } from './slice.js';
 
 export const store = configureStore({
   reducer: contactsReducer,
